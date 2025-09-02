386d21590029296f8397374974fc137245ef84de
arunsla <arunraj.softlogic@gmail.com>
2025-09-02 16:51:49 +0530

    update

---
+++ js/day2/java.js
@@ -0,0 +1,168 @@
+// let sla = "students";
+// sla = "friends";
+// console.log(sla);
+// const las = "brick"
+// las = ""
+
+// DataTypes 
+
+
+//string
+// let name = "PREM";
+// let greeting = 'Hello';
+// let Message = `Hi, ${name}`;
+
+
+// console.log(name);
+// console.log(greeting);
+// console.log(Message);
+
+
+
+
+//NUMBERS
+
+// let age = 28;
+// let price = 19.99;
+// let infinityValue = Infinity;
+// let notANumber = NaN;
+// console.log(infinityValue);
+
+//bigINt
+// let big = 12345789012345678901234567890n;
+// console.log(big+1n);
+
+//boolean
+
+// let isStudent = true ;
+// let isLoggedIn = false ;
+
+// console.log(typeof isLoggedIN);
+
+//undefined 
+// let city ;
+// console.log(typeof city);
+
+//NULL
+
+// Intentional absence of a Value;
+// let car = null;
+// console.log(car);
+
+
+
+//symbol
+
+
+// let id1 = Symbol("id");
+// let id2 = Symbol("id");
+
+// console.log(id1 == id2);
+
+//Primitive Data Type in JS 
+
+
+// Symbol 
+// numbers
+// String
+// BigInt
+// null
+// Boolean
+// Undefined 
+
+//Non-primitive 
+
+// Object
+// Array
+// Data 
+// Function
+
+// let person = {
+//     name : "Meena",
+//     age:18,
+// }
+// console.log(person.name);
+// console.log(person.age);
+
+//Array
+
+// let Color = ["Red", "Green", "Yellow", "Pink"]
+
+
+// console.log(Color[0]);
+
+
+
+//function 
+
+// function greet(){
+//     return "helo"
+// }
+// console.log(greet());
+
+//Date 
+
+// let today = new Date ;
+// console.log( typeof today);
+
+
+
+//String
+
+let today = "sunday"
+
+console.log(today);
+//number
+
+const a = 12;
+console.log(a);
+
+
+//bigint
+
+let big = 2101183083018201830n
+console.log(big+1n)
+
+//array
+let abc = ["red", "black", 12];
+console.log(abc[2]);
+ // object
+
+ let abcd={
+    name : "Sanjay",
+    age : 100,
+ }
+ console.log(abcd.age);
+console.log(abcd.name)
+
+let isString=true;
+let isLoggedIN=false;
+console.log(typeof isLoggedIN)
+
+function apple(a,b){
+    
+    return (a+b)
+}
+console.log(apple(10,20))
+
+
+// date
+
+// let ht = new Date;
+// console.log(  typeof ht);
+// let c = 15;
+// let new_c = BigInt(c);
+// let b = 6959575748394958658n;
+// function addition(new_c, )
+
+// function
+// console.log(d + e);
+// console.log(b);
+// function addition(a, )
+
+
+
+
+
+
+
