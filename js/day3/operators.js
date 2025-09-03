4487958587b484d1cd641c5eddadf24d14a98b1e
arunsla <arunraj.softlogic@gmail.com>
2025-09-03 16:33:59 +0530

    JS day 3

---
+++ js/day3/operators.js
@@ -0,0 +1,95 @@
+// const a = 10;
+// const b = 5;
+
+
+// //addition
+// console.log(a+b);
+// //subtraction
+// console.log(a-b);
+// //multiplication 
+// console.log(a*b);
+// //division(float)
+// console.log(a/b);
+// // //floor division 
+// // console.log(a //b);  ===> math.floor()
+// console.log(a%b);
+// //exponentiation 
+// console.log(a**b);
+
+
+// //Assignment Operators 
+// assignment operators ARE USED TO ASSIGN VALUES 
+
+// const ab = 5;
+// const bc = 30;
+// const x = 10;
+// let x = 0;
+// += 
+// x = 5 ;
+// x += 3 ;
+// console.log(x);
+
+//  let x ;
+// x = 5;
+// x -= 3;
+
+// x = 5;
+// x /= 3;
+
+
+// console.log(x);
+
+
+
+
+
+// x = 5;
+// x %= 2;
+
+
+// console.log(" x=5; x %=2", x);
+
+
+
+// let a = 5;
+// let b = 3;
+// console.log("a=",a, "b=", b);
+// console.log(a == b);
+// console.log(a != b);
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a >= b);
+// console.log(a <= b);
+
+let a = 5;
+let b = 10; 
+
+
+console.log(a > 3 || b> 5);// or
+console.log( a > 6 && b > 5); // and
+// console.log(a && 0 );
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
