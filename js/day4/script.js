9751fc7bb13d0c944925f5e814e10cd4f61ec753
arunsla <arunraj.softlogic@gmail.com>
2025-09-04 18:11:26 +0530

    day 4 js

---
+++ js/day4/script.js
@@ -0,0 +1,56 @@
+// conditional Statements
+// let ab = 20;
+
+// if statement
+
+// let age = 18;
+
+// if (age >= 18){
+//     console.log("you are an adult");
+// }
+
+// let mark = 200;
+
+// if (mark > 150){
+//     console.log("you have scored highest mark ");
+
+// }
+
+// if...else
+
+// let age = 18;
+
+// if (age >= 18){
+//     console.log("You can vote ");
+// }else {
+//     console.log("You cannot vote becoz u r so Young");
+
+// }
+
+// if...else  if..else
+
+// let marks = 45;
+
+// if (marks >= 90){
+//     console.log("Grade A");
+// }else if (marks >= 80) {
+//     console.log("Grade B");
+// }else if(marks >= 50 ){
+//     console.log("Grade C");
+// } else{
+//     console.log("You are stupid");
+
+// }
+
+let day = "friday";
+
+switch (day) {
+  case "monday":
+    console.log("Monday");
+    break;
+  case "tuesday":
+    console.log("tuesday");
+    break;
+  default:
+    console.log("Invalid day");
+}
