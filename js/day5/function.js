008b69d68082651022403d84d555d28564151ce5
arunsla <arunraj.softlogic@gmail.com>
2025-09-08 17:07:39 +0530

    update

---
+++ js/day5/function.js
@@ -0,0 +1,39 @@
+//defining a function 
+
+
+
+// function name (){
+//     console.log("hello function"); 
+// }
+
+// name();
+
+
+
+
+// function with paramaeters
+
+
+
+// function name (ar){
+//     console.log("hello" + ar);
+// }
+
+
+// name(" thiyagu");
+// name(" arunraj");
+// name(" arun");
+
+
+
+
+// function add (b1,b2){
+//     console.log("hello " + b1*b2);
+
+// }
+// add(10,20);
+// add(100,200);
+// add(1,2);
+
+
+
