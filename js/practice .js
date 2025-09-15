5c4f29a44d382b0f93ab008a6e221af74198ef5c
arunsla <arunraj.softlogic@gmail.com>
2025-09-15 17:12:26 +0530

    <title>aradsjgsdg</title>

---
+++ js/practice .js
@@ -0,0 +1,4 @@
+// let aaaa = document.getElementById("heading");
+
+// aaaa.style.color = "red";
+// aaaa.innerHTML = "<p>lorem30</p>"
--- js/secondsaturday/js.js
+++ js/secondsaturday/js.js
@@ -1,30 +1,30 @@
 
-// document Properties 
+// // document Properties 
 
-console.log(document.title);
-console.log(document.URL);
-console.log(document.domain);
-console.log(document.head);
-console.log(document.body);
-console.log(document.links);
-console.log(document.images);
-console.log(document.forms);
-console.log(document.scripts);
-
-
-// element  Node Properties 
+// console.log(document.title);
+// console.log(document.URL);
+// console.log(document.domain);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.links);
+// console.log(document.images);
+// console.log(document.forms);
+// console.log(document.scripts);
 
 
+// // element  Node Properties 
 
 
-let element= document.getElementById("demo")
+
+
+// let element= document.getElementById("demo")
  
 
-console.log(element.id);    
-console.log(element.className);
-console.log(element.tagName);
-console.log(element.innerHTML); // inner HTML
-console.log(element.textContent); // text content
+// console.log(element.id);    
+// console.log(element.className);
+// console.log(element.tagName);
+// console.log(element.innerHTML); // inner HTML
+// console.log(element.textContent); // text content
 
 
 
